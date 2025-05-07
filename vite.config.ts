a016dd4a380fba69ba82aba1a11d7b3640d34f1a
ryandthomson <ryandthomson@gmail.com>
2025-05-07 01:56:26 +0000

    Add vite.config.js to enable Svelte/Vite dev

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [svelte()]
+});
